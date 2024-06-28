7e08d9cd571fe9ca2c6a76aec283b097f53a1900
LacChinhSoft <128137927+binhkid2@users.noreply.github.com>
2024-06-28 03:40:58 +0000

    authcheck sub too

--- lib/AuthCheck.tsx
+++ lib/AuthCheck.tsx
@@ -1,13 +1,30 @@
  
   'use client'
-import { useAtom } from 'jotai';
+import { createStore, useAtom } from 'jotai';
 import './AuthCheck.scss'
 import ButtonLogin from "./ButtonLogin";
 import { isAuthenticatedStore } from './store';
+import { useEffect } from 'react';
 //@ts-ignore
   
 export default  function AuthCheck({children}) { 
-  const [isAuthenticated, ] = useAtom(isAuthenticatedStore);
+  const [isAuthenticated,setIsAuthenticated ] = useAtom(isAuthenticatedStore);
+  const myStore = createStore();
+  useEffect(() => {
+ 
+
+    // Subscribe to changes in isAuthenticatedStore
+    const unsubIsAuthenticated = myStore.sub(isAuthenticatedStore, () => {
+      const newIsAuthenticated = myStore.get(isAuthenticatedStore);
+      console.log('isAuthenticatedStore value is changed to', newIsAuthenticated);
+      setIsAuthenticated(newIsAuthenticated);
+    });
+
+    // Clean up subscriptions on unmount
+    return () => { 
+      unsubIsAuthenticated();
+    };
+  }, [myStore, setIsAuthenticated]);
   return (
     <>
      {!isAuthenticated ? (
